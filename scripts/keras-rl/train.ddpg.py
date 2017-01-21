a672445c5f34baef45ce4e3c90c31407b49cd55c
Viktor Makoviychuk <vmakoviychuk@nvidia.com>
2017-01-21 19:24:46 +0300

    Added DDPG agent with improved actor and critic architectures. Small clean-up. Exposed additional training parameters.

--- scripts/keras-rl/train.ddpg.py
+++ scripts/keras-rl/train.ddpg.py
@@ -4,7 +4,7 @@ import sys
 
 from keras.models import Sequential, Model
 from keras.layers import Dense, Activation, Flatten, Input, merge
-from keras.optimizers import Adam
+from keras.optimizers import Adam, Adadelta, RMSpropá Nadam
 
 import numpy as np
 
@@ -19,16 +19,21 @@ from keras.optimizers import RMSprop
 import argparse
 import math
 
-# Some meta parameters
-nallsteps = 10000000
 
 # Command line parameters
 parser = argparse.ArgumentParser(description='Train or test neural net motor controller')
 parser.add_argument('--train', dest='train', action='store_true', default=True)
 parser.add_argument('--test', dest='train', action='store_false', default=True)
 parser.add_argument('--visualize', dest='visualize', action='store_true', default=False)
-parser.add_argument('--output', dest='output', action='store', default="model.h5f")
+parser.add_argument('--output', dest='output', action='store', default=None)
 parser.add_argument('--env', dest='env', action='store', default="Arm")
+parser.add_argument('--sigma', dest='sigma', action='store', default=0.25)
+parser.add_argument('--theta', dest='theta', action='store', default=0.15)
+parser.add_argument('--gamma', dest='gamma', action='store', default=0.99)
+parser.add_argument('--target_model_update', dest='target_model_update', action='store', default=1e-3, type=float)
+parser.add_argument('--nb_steps', dest='nb_steps', action='store', default=1500000, type=int)
+parser.add_argument('--rseed', dest='rseed', action='store', default=random.randint(0, 4294967295), type=int)
+
 args = parser.parse_args()
 
 if args.env == "Gait":
@@ -38,16 +43,20 @@ else:
 
 nb_actions = env.action_space.shape[0]
 
-# Next, we build a very simple model.
+init_name='lecun_uniform'
+
+# Next, we build a simple model.
 actor = Sequential()
 actor.add(Flatten(input_shape=(1,) + env.observation_space.shape))
-actor.add(Dense(32))
+actor.add(Dense(24, init = init_name))
 actor.add(Activation('relu'))
-actor.add(Dense(32))
+actor.add(Dense(24, init = init_name))
 actor.add(Activation('relu'))
-actor.add(Dense(32))
+actor.add(Dense(24, init = init_name))
 actor.add(Activation('relu'))
-actor.add(Dense(nb_actions))
+actor.add(Dense(24, init = init_name))
+actor.add(Activation('relu'))
+actor.add(Dense(nb_actions, init = init_name))
 actor.add(Activation('sigmoid'))
 print(actor.summary())
 
@@ -55,11 +64,13 @@ action_input = Input(shape=(nb_actions,), name='action_input')
 observation_input = Input(shape=(1,) + env.observation_space.shape, name='observation_input')
 flattened_observation = Flatten()(observation_input)
 x = merge([action_input, flattened_observation], mode='concat')
-x = Dense(64)(x)
+x = Dense(48, init = init_name)(x)
 x = Activation('relu')(x)
-x = Dense(64)(x)
+x = Dense(48, init = init_name)(x)
 x = Activation('relu')(x)
-x = Dense(64)(x)
+x = Dense(48, init = init_name)(x)
+x = Activation('relu')(x)
+x = Dense(48, init = init_name)(x)
 x = Activation('relu')(x)
 x = Dense(1)(x)
 x = Activation('linear')(x)
@@ -68,23 +79,20 @@ print(critic.summary())
 
 # Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
 # even the metrics!
-memory = SequentialMemory(limit=100000, window_length=1)
-random_process = OrnsteinUhlenbeckProcess(theta=.15, mu=0., sigma=.2, size=env.noutput)
+memory = SequentialMemory(limit=200000, window_length=1)
+random_process = OrnsteinUhlenbeckProcess(theta=float(args.theta), mu=0., sigma=float(args.sigma), size=env.noutput)
 agent = DDPGAgent(nb_actions=nb_actions, actor=actor, critic=critic, critic_action_input=action_input,
                   memory=memory, nb_steps_warmup_critic=100, nb_steps_warmup_actor=100,
-                  random_process=random_process, gamma=.99, target_model_update=1e-3,
+                  random_process=random_process, gamma=float(args.gamma), batch_size=32, target_model_update=float(args.target_model_update),
                   delta_range=(-100., 100.))
-# agent = ContinuousDQNAgent(nb_actions=env.noutput, V_model=V_model, L_model=L_model, mu_model=mu_model,
-#                            memory=memory, nb_steps_warmup=1000, random_process=random_process,
-#                            gamma=.99, target_model_update=0.1)
-#agent.compile(Adam(lr=.001, clipnorm=1.), metrics=['mae'])
-agent.compile([RMSprop(lr=.001), RMSprop(lr=.001)], metrics=['mae'])
+
+agent.compile([Nadam(lr=0.001), Nadam(lr=0.002)], metrics=['mae'])
 
 # Okay, now it's time to learn something! We visualize the training here for show, but this
 # slows down training quite a lot. You can always safely abort the training prematurely using
 # Ctrl + C.
 if args.train:
-    agent.fit(env, nb_steps=nallsteps, visualize=True, verbose=1, nb_max_episode_steps=env.timestep_limit, log_interval=10000)
+    agent.fit(env, nb_steps=env.nb_steps, visualize=True, verbose=1, nb_max_episode_steps=env.timestep_limit, log_interval=10000)
     # After training is done, we save the final weights.
     agent.save_weights(args.output, overwrite=True)
 
@@ -93,7 +101,7 @@ if not args.train:
     # Finally, evaluate our algorithm for 5 episodes.
 
     if args.env != "Arm":
-        agent.test(env, nb_episodes=5, visualize=True, nb_max_episode_steps=500)
+        agent.test(env, nb_episodes=5, visualize=True, nb_max_episode_steps=10000)
     else:
         for i in range(10000):
             if i % 300 == 0:
