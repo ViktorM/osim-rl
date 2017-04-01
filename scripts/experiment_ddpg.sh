6619beb67b2c3a6fb042d47893a8f24806e65bf3
ViktorM <victor.makoviychuk@gmail.com>
2017-04-02 00:32:05 +0300

    Added DDPG and CDQN experiment scripts.

---
+++ scripts/experiment_ddpg.sh
@@ -0,0 +1,3 @@
+python ddpg.py --train --rseed 13 --steps 1000000 --sigma 0.25 --theta 0.15
+python ddpg.py --train --rseed 197 --steps 1000000 --sigma 0.25 --theta 0.15
+python ddpg.py --train --rseed 84629 --steps 1000000 --sigma 0.25 --theta 0.15
